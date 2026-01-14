d5d629200746bcbed7c40c1956d3d9beb695bd5f
samaelbakry <samaabakry68@gmail.com>
2026-01-14 14:21:03 +0200

    Implemented navbar and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import gsap from "gsap";
-import{ScrollTrigger , SplitText}from "gsap/all"
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Navbar/Hero/Hero";
 
-gsap.registerPlugin(ScrollTrigger , SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
+  return (
+    <>
+      <main>
+        <Navbar />
+        <Hero />
+		<div className="bg-black min-h-screen">
 
-	return <>
-	<h1 className=" flex-center">hello</h1>
-	</>
+		</div>
+      </main>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import gsap from "gsap";
+import logoImage from "/images/logo.png"
+import { useGSAP } from "@gsap/react";
+
+export default function Navbar() {
+    useGSAP(()=>{
+        const tween = gsap.timeline({
+            scrollTrigger:{
+            trigger:"nav",
+            start:"bottom top"
+            }})
+            tween.fromTo("nav" ,{ backgroundColor:"transparent" },
+
+                { backgroundColor:"#00000050",
+                backgroundFilter:"blur(10px)",
+                duration:"1s",
+                ease:"power2"
+                }
+            )
+        
+    },[])
+  const links = [
+    {
+      id: "cocktails",
+      title: "Cocktails",
+    },
+    {
+      id: "about",
+      title: "About Us",
+    },
+    {
+      id: "work",
+      title: "The Art",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  return (
+    <>
+      <nav>
+        <div className="max-w-7xl">
+          <a className="flex items-center gap-2">
+            <img src={logoImage} alt="website-logo" />
+            <span>velvet pour</span>
+          </a>
+          <ul>
+            {links.map((link) => (
+              <li key={link.id}>
+                <a  href={`${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+     
+    </>
+  );
+}
